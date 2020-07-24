f04f0898b747229fe4f08abedaf752e4152e7088
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2020-07-24 08:54:09 +0200

    etcd: added firewall mode option

--- manifests/etcd.pp
+++ manifests/etcd.pp
@@ -1,3 +1,8 @@
+# @summary Configures ETCD
+#
+# @param firewall_mode
+#   Open swarm ports to whole cluster_iface ('interface') or for each other peer node separately ('peer').
+#
 class g_kubernetes::etcd(
   Hash[String, Stdlib::Host] $servers,
   Stdlib::Host $cluster_addr,
@@ -19,6 +24,7 @@ class g_kubernetes::etcd(
   Optional[String] $package_checksum = undef,
   Boolean $service_manage = true,
   Boolean $manage_firewall = true,
+  Enum['interface', 'peer'] $firewall_mode = 'interface',
 
   Optional[G_kubernetes::CertSource] $client_ca_cert = undef,
   Optional[G_kubernetes::CertSource] $client_cert = undef,
@@ -51,17 +57,84 @@ class g_kubernetes::etcd(
   include ::g_kubernetes::etcd::config
 
   if $manage_firewall {
-    ['peer', 'client'].each | $type | {
-      $side = getvar("::g_kubernetes::etcd::${type}_side")
-      $port = getvar("::g_kubernetes::etcd::${type}_port")
+    $rule_config = {
+      'proto'  => tcp,
+      'action' => accept,
+    }
 
-      g_server::get_interfaces($side).each | $iface | {
-        g_firewall { "006 Allow inbound ETCD ${type} from ${iface}":
-          dport   => $port,
-          proto   => tcp,
-          action  => accept,
+    if $firewall_mode == 'interface' {
+      ['peer', 'client'].each | $type | {
+        $side = getvar("::g_kubernetes::etcd::${type}_side")
+        $port = getvar("::g_kubernetes::etcd::${type}_port")
+
+        g_server::get_interfaces($side).each | $iface | {
+          g_firewall { "006 Allow inbound ETCD ${type} from ${iface}":
+            dport   => $port,
+            iniface => $iface,
+            *       => $rule_config
+          }
+        }
+      }
+    } else {
+      g_server::get_interfaces($::g_kubernetes::etcd::client_side).each | $iface | {
+        g_firewall { "006 Allow inbound ETCD client from ${iface}":
+          dport   => $::g_kubernetes::etcd::client_port,
+          iniface => $iface,
+          *       => $rule_config
+        }
+      }
+
+      firewallchain { 'ETCD-PEER:filter:IPv4': purge  => true }
+      firewallchain { 'ETCD-PEER:filter:IPv6': purge  => true }
+
+      g_server::get_interfaces($::g_kubernetes::etcd::peer_side).each | $iface | {
+        g_firewall { "006 Allow inbound ETCD peer from nodes on ${iface}":
+          jump    => 'ETCD-PEER',
           iniface => $iface
         }
+      }
+
+      $ips = g_server::get_interfaces($::g_kubernetes::etcd::peer_side).map | $iface | {
+        $_network_info = $::facts['networking']['interfaces'][$iface]
+        Hash(['', '6'].map |$t| {
+          [
+            $t,
+            $_network_info["bindings${t}"].map | $c | {
+              $c['address']
+            }
+          ]
+        })
+      }.reduce({'' => [], '6' => []}) | $memo, $c | {
+        {
+          '' => $memo[''] + $c[''],
+          '6' => $memo['6'] + $c['6']
+        }
+      }
+
+      $config = merge($rule_config, {
+        dport => $::g_kubernetes::etcd::peer_port,
+        tag => ['g_kubernetes::etcd::peer']
+      })
+      $rule_name = "006 Allow inbound ETCD peer from ${::fqdn}"
+
+      if ($ips[''].length > 0) {
+        @@g_firewall::ipv4 { $rule_name:
+          source => $ips[''],
+          *      => $config
+        }
+      }
+      if ($ips['6'].length > 0) {
+        @@g_firewall::ipv6 { $rule_name:
+          source => $ips['6'],
+          *      => $config
+        }
+      }
+
+      G_firewall::Ipv4 <<| tag == 'g_kubernetes::etcd::peer' |>> {
+        chain  => 'ETCD-PEER'
+      }
+      G_firewall::Ipv6 <<| tag == 'g_kubernetes::etcd::peer' |>> {
+        chain  => 'ETCD-PEER'
       }
     }
   }
