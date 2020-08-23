c49e9eccf5cc4b91c67f27d498ed2cbb22a7f50d
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2020-08-23 21:52:26 +0200

    etcd, vault: get ip addrs from g_server if available to create proper config on first run

--- manifests/etcd.pp
+++ manifests/etcd.pp
@@ -62,13 +62,21 @@ class g_kubernetes::etcd(
   $_ips = ['peer', 'client'].map | $type | {
     $side = getvar("${type}_side")
     flatten(g_server::get_interfaces($side).map | $iface | {
-      $_network_info = $::facts['networking']['interfaces'][$iface]
-      ['', '6'].map |$t| {
-        $_network_info["bindings${t}"].map | $c | {
-          $c['address']
-        }.filter | $i | {
-          $i !~ G_kubernetes::Ipv6LinkLocal
+      $addrs = delete_undef_values([
+        G_server::Network::Iface[$iface]['ipv4addr'],
+        G_server::Network::Iface[$iface]['ipv6addr']
+      ])
+      if $addrs.empty() {
+        $_network_info = $::facts['networking']['interfaces'][$iface]
+        ['', '6'].map |$t| {
+          $_network_info["bindings${t}"].map | $c | {
+            $c['address']
+          }.filter | $i | {
+            $i !~ G_kubernetes::Ipv6LinkLocal
+          }
         }
+      } else {
+        $addrs
       }
     })
   }
